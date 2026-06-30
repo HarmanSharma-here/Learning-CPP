02305720995153712eb08bf3484e147415989b32
HarmanSharma-here <HarmanSharma.here@gmail.com>
2026-06-30 17:19:30 +0530

    Implement basic calculator functionality
    Fix division by zero check and output formatting.

---
+++ 02_basic_calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double num1,num2;
+  
+  cin>>num1;
+  cin>>num2;
+  cout<<num1+num2<<endl;
+  cout<<num1-num2<<endl;
+  cout<<num1*num2;
+
+  if (num2!=0);
+{
+  cout<<num1/num2<<endl;
+}
+  else
+{
+  cout<<"Error"<<endl;
+}
+  
+  return 0;
+}
